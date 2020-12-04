18ad36bd1c01699348fddd1b52dfed98574fa5e2
manan012 <manangoel012@gmail.com>
2020-12-04 13:52:06 +0530

    basic layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/main.scss";
+
+
+class App extends React.Component {
+
+  static async getInitialProps({Component, ctx}) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />
+  }
 }
 
-export default MyApp
+export default App;
